a09a78cca3d58cb793366b27c4628b45155f9b23
Joerg Roth <joerg.roth@geo.uzh.ch>
2022-02-09 20:36:28 +0100

    improve search

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,8 +7,8 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+        appendString += '<p><a href="' + item.url + '">' + item.title + '</a>';
+        appendString += '<br/>' + item.content.substring(0, 250) + '...</p>';
       }
 
       searchResults.innerHTML = appendString;
@@ -41,6 +41,7 @@
       this.field('id');
       this.field('title', { boost: 10 });
       this.field('author');
+      this.field('journal');
       this.field('category');
       this.field('content');
 
@@ -50,6 +51,7 @@
           'id': key,
           'title': window.store[key].title,
           'author': window.store[key].author,
+          'journal': window.store[key].journal,
           'category': window.store[key].category,
           'content': window.store[key].content
         });
